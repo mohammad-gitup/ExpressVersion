37c163cb81f14c80ff1111e00fd9d6bd657111c7
yashnevatia <yash1997@hku.hk>
2017-07-12 13:48:24 -0700

    here

--- public/script.js
+++ public/script.js
@@ -1,53 +1,54 @@
 $(document).ready(function() {
 
   var socket = io();
+
   socket.on('connect', function() {
     console.log('Connected!');
 
-    socket.emit('spotifySetup', localStorage.getItem("spotifyId"));
+  });
 
-    socket.on('getRefreshToken', function(refreshToken){
-        localStorage.setItem("refreshToken",refreshToken);
-        setInterval(function(){
-          socket.emit("toRefresh", localStorage.getItem("refreshToken"));
-        },60000*30);
+  socket.emit('spotifySetup', localStorage.getItem("spotifyId"));
+
+  socket.on('getRefreshToken', function(refreshToken){
+      localStorage.setItem("refreshToken",refreshToken);
+      setInterval(function(){
+        socket.emit("toRefresh", localStorage.getItem("refreshToken"));
+      },60000*30);
+  })
+
+  socket.on('getAccessToken', function(userAccessToken) {
+      localStorage.setItem("accessToken", userAccessToken);
     })
 
-    socket.on('getAccessToken', function(userAccessToken) {
-        localStorage.setItem("accessToken", userAccessToken);
+  socket.on('DJSetting', function(data) {
+
+      console.log("reaching dj setting");
+      var songProgress = data.a;
+      var songURI = data.b;
+      //ajax call will be done here
+      $.ajax({
+        url:'https://api.spotify.com/v1/me/player/play',
+        method:'PUT',
+        headers: { 'Authorization':'Bearer ' + localStorage.getItem("accessToken"), "Content-Type": "application/json"},
+        data: JSON.stringify({
+          "uris" :[songURI]
+        }),
+        dataType: "JSON",
+        success: function(data){
+          console.log("this is it bitch");
+          $.ajax({
+            url: `https://api.spotify.com/v1/me/player/seek?position_ms=${songProgress}`,
+            headers: { 'Authorization': 'Bearer ' +  localStorage.getItem("accessToken")},
+            method:'put',
+            json:true,
+            success:function(something){
+              console.log("lets go home");
+            }
+
+          })
+        }
       })
-
-    socket.on('DJSetting', function(data) {
-
-        console.log("reaching dj setting");
-        var songProgress = data.a;
-        var songURI = data.b;
-        //ajax call will be done here
-        $.ajax({
-          url:'https://api.spotify.com/v1/me/player/play',
-          method:'PUT',
-          headers: { 'Authorization':'Bearer ' + localStorage.getItem("accessToken"), "Content-Type": "application/json"},
-          data: JSON.stringify({
-            "uris" :[songURI]
-          }),
-          dataType: "JSON",
-          success: function(data){
-            console.log("this is it bitch");
-            $.ajax({
-              url: `https://api.spotify.com/v1/me/player/seek?position_ms=${songProgress}`,
-              headers: { 'Authorization': 'Bearer ' +  localStorage.getItem("accessToken")},
-              method:'put',
-              json:true,
-              success:function(something){
-                console.log("lets go home");
-              }
-
-            })
-          }
-        })
-      })
-
-  });
+    })
 
   $('#createRoom').on('click' ,function(event) {
     event.preventDefault();
