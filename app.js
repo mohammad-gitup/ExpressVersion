ef44e4510c04498624431f1b64690186658a914d
yashnevatia <yash1997@hku.hk>
2017-07-14 12:29:39 -0700

    fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //ADDED JULY 13TH TO LOAD FILES EXTERNALLY
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 //passport stuff
 
