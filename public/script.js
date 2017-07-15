9542a02a4570ae3c8708453e1fdc5f24f356db58
yashnevatia <yash1997@hku.hk>
2017-07-14 17:22:34 -0700

    serach test

--- public/script.js
+++ public/script.js
@@ -109,6 +109,25 @@ $(document).ready(function() {
         listofRooms.push(key);
       }
     }
+    function myFunction() {
+        // Declare variables
+        var input, filter, ul, li, a, i;
+        input = document.getElementById('myInput');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("listofRooms");
+        li = ul.getElementsByTagName('li');
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("button")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    };
+
     console.log(listofRooms);
     $('.wrapper').empty();
     $('.wrapper').append(`<h3 style="color:white">List of available rooms</h3>`);
@@ -149,25 +168,6 @@ $(document).ready(function() {
   $('#closeRoom').on('click',function(){
     var id=$(this).attr('data-id');
   })
-
-  function myFunction() {
-      // Declare variables
-      var input, filter, ul, li, a, i;
-      input = document.getElementById('myInput');
-      filter = input.value.toUpperCase();
-      ul = document.getElementById("listofRooms");
-      li = ul.getElementsByTagName('li');
-
-      // Loop through all list items, and hide those who don't match the search query
-      for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("button")[0];
-          if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-              li[i].style.display = "";
-          } else {
-              li[i].style.display = "none";
-          }
-      }
-  };
 
 
 });
