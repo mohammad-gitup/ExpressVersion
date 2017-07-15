38d3b3a34ec77ceb8b7ed49a095f4040e6d7984b
yashnevatia <yash1997@hku.hk>
2017-07-15 01:24:33 -0700

    here

--- public/script.js
+++ public/script.js
@@ -48,22 +48,22 @@ $(document).ready(function() {
     })
   })
 
-  $('.wrapper').on('click' , '#createRoom ',function(event) {
-    $('.wrapper').empty();
-    var createTemplate = $(
-      `<div class="main">
-      <div class="container-fluid">
-        <div class="center topLevel" style="width: 60%; margin-right: auto; margin-left: auto;">
-          <input name="roomNameBar" id="roomName" placeholder="Enter a room name here" class="ghost-input text-center"></input>
+$('.wrapper').on('click' , '#createRoom ',function(event) {
+      $('.wrapper').empty();
+      var createTemplate = $(
+        `<div class="main">
+        <div class="container-fluid">
+          <div class="center topLevel" style="width: 60%; margin-right: auto; margin-left: auto;">
+            <input name="roomNameBar" id="roomName" placeholder="Enter a room name here" class="ghost-input text-center"></input>
+          </div>
+          <div class="center" >
+            <h1 class="text-center text label-text boxed raise" name="button" id="startRoom">Go Live</h1>
+          </div>
         </div>
-        <div class="center" >
-          <h1 class="text-center text label-text boxed raise" name="button" id="startRoom">Go Live</h1>
-        </div>
-      </div>
-    </div>`
-  )
-  $('.wrapper').append(createTemplate);
-})
+      </div>`
+    );
+      $('.wrapper').append(createTemplate);
+  })
 
 $('.wrapper').on('click', '#startRoom', function(event){
   event.preventDefault();
@@ -117,10 +117,10 @@ $('.wrapper').on('click', '.leaveRoom', function(event){
   })
 
   $('.wrapper').on('click', '.passDJ', function(event){
-      var newDjUsername = $(this).attr('data-id');
-      console.log("new dj name", newDjUsername);
-      socket.emit('newDj', newDjUsername);
-      socket.emit('leaveRoomDj');
+    var newDjUsername = $(this).attr('data-id');
+    console.log("new dj name", newDjUsername);
+    socket.emit('newDj', newDjUsername);
+    socket.emit('leaveRoomDj');
   })
 
 
@@ -141,50 +141,48 @@ $('.wrapper').on('click', '.leaveRoom', function(event){
 
     <ul class="lastSongs">
 
-          </ul >
-          <button type="button" class="leaveRoom" data-id="${roomInfo.room}">Leave room</button>
-      </div>`;
+    </ul >
+    <button type="button" class="leaveRoom" data-id="${roomInfo.room}">Leave room</button>
+  </div>`;
 
-      $('.wrapper').empty();
-      $('.wrapper').append(djRoom);
-      var users = roomInfo.listeners;
-      for(var i=0 ;i<users.length; i++){
-        var userObj = users[i];
-        $('.activeUsersforUser').append(`<li> | ${userObj.username} | <img src=${userObj.imageURL}> </li>`);
-      }
+  $('.wrapper').empty();
+  $('.wrapper').append(djRoom);
+  var users = roomInfo.listeners;
+  for(var i=0 ;i<users.length; i++){
+    var userObj = users[i];
+    $('.activeUsersforUser').append(`<li> | ${userObj.username} | <img src=${userObj.imageURL}> </li>`);
+  }
 
-  })
+})
 
-  socket.on('rooms', function(rooms){
-    console.log(rooms);
-    var listofRooms = [];
-    for(var key in rooms){
-      if(rooms[key].hasOwnProperty('DJToken')){
-        listofRooms.push(key);
-      }
+socket.on('rooms', function(rooms){
+  console.log(rooms);
+  var listofRooms = [];
+  for(var key in rooms){
+    if(rooms[key].hasOwnProperty('DJToken')){
+      listofRooms.push(key);
     }
-  })
-
-
+  }
   console.log(listofRooms);
   $('.wrapper').empty();
   // $('.wrapper').append(`<h1 class="text headertext"></h1>`);
+    $('.wrapper').append(`<div class="center">
+    <h1 class="text-center text">There are ${listofRooms.length} rooms to browse</h1>
+    </div>
+
+  <div class="center" style="width: 40%; margin-right: auto; margin-left: auto;">
+    <input id="myInput" style="font-size: 30px;" placeholder="Search for a room..." class="ghost-input text-center" id="myInput" onkeyup="searchFunction()"></input>
+  </div>`)
+  $('.wrapper').append(`<div id="listofRooms" class="text-center standard-text" style="width: 35%; margin-right: auto; margin-left: auto; overflow: auto;"></div>`);
+
+  for(var i =0  ; i< listofRooms.length ;i++){
+    var roomItem = $(`<h1 class="raise-room joinexistingRoom" data-id='${listofRooms[i]}'>${listofRooms[i]}</h1>`);
+    $('#listofRooms').append(roomItem);
+  }
+
   $('.wrapper').append(`<div class="center">
-  <h1 class="text-center text">There are ${listofRooms.length} rooms to browse</h1>
-</div>
-
-<div class="center" style="width: 40%; margin-right: auto; margin-left: auto;">
-  <input id="myInput" style="font-size: 30px;" placeholder="Search for a room..." class="ghost-input text-center" id="myInput" onkeyup="searchFunction()"></input>
-</div>`)
-$('.wrapper').append(`<div id="listofRooms" class="text-center standard-text" style="width: 35%; margin-right: auto; margin-left: auto; overflow: auto;"></div>`);
-
-for(var i =0  ; i< listofRooms.length ;i++){
-  var roomItem = $(`<h1 class="raise-room joinexistingRoom" data-id='${listofRooms[i]}'>${listofRooms[i]}</h1>`);
-  $('#listofRooms').append(roomItem);
-}
-$('.wrapper').append(`<div class="center">
-		<h1 class="text-center text middle boxed raise">Or start your own party</h1>
-	</div>`);
+  <h1 class="text-center text middle boxed raise">Or start your own party</h1>
+  </div>`);
 
 
 })
@@ -209,29 +207,30 @@ socket.on('djRoomInfo', function(info) {
   </ul >
 
   <button type="button" class="closeRoom" data-id="${info.room}">Close room</button>
-</div>`;
-$('.wrapper').empty();
-$('.wrapper').append(djRoom);
+  </div>`;
+  $('.wrapper').empty();
+  $('.wrapper').append(djRoom);
+}
 
-  socket.on('newDjRoomInfo', function(info){
-    var djRoom=`<div>
-    <div>
-      Room Name ${info.room}
-    </div>
+socket.on('newDjRoomInfo', function(info){
+  var djRoom=`<div>
+  <div>
+    Room Name ${info.room}
+  </div>
 
-    <div>
-      Dj Photo: <img src="${info.djPhoto}" style="width:304px;height:228px;">
-    </div>
+  <div>
+    Dj Photo: <img src="${info.djPhoto}" style="width:304px;height:228px;">
+  </div>
 
-    <ul class="activeUsers">
+  <ul class="activeUsers">
 
-    </ul>
+  </ul>
 
-    <ul class="lastSongs">
+  <ul class="lastSongs">
 
-    </ul >
+  </ul >
 
-    <button type="button" class="closeRoom" data-id="${info.room}">Close room</button>
+  <button type="button" class="closeRoom" data-id="${info.room}">Close room</button>
   </div>`;
   $('.wrapper').empty();
   $('.wrapper').append(djRoom);
@@ -241,23 +240,23 @@ $('.wrapper').append(djRoom);
     $('.activeUsers').append(`<li> | ${userObj.username} | <img src=${userObj.imageURL}> </li>`);
   }
 
-  })
+})
 
-  socket.on('disconnectFromRoom', function(roomName) {
-    socket.emit('leaveRoom', roomName);
-    var home = `<div class="container-fluid" >
-        	<div style="display: flex; justify-content: center; margin-left: 50%; margin-right: auto; margin-top: 5%">
-        		<a class="topLevel text" id="createRoom" ><span class="text-center raise boxed headertext text">Create</span></a>
-        		<img src="/static/images/leftaux.svg" class="img-responsive" style="position: sticky; margin-top: 10%;">
-        	</div>
-        		<br><br>
-        	<h2 class="text middle">OR</h2>
-        		<br><br>
-        		<div style="display: flex; justify-content: center; margin-right: 50%; margin-left: auto;">
-        			<img style="position: sticky;" class="img-responsive text" src="/static/images/rightaux.svg" alt="">
-        			<a class="text" id="joinRoom"><span class="text-center raise boxed text headertext"><span class="text headertext" style="opacity: 0">1</span>Join<span class="text headertext" style="opacity: 0">1</span></span></a>
-        		</div>
-        </div>`;
+socket.on('disconnectFromRoom', function(roomName) {
+  socket.emit('leaveRoom', roomName);
+  var home = `<div class="container-fluid" >
+  <div style="display: flex; justify-content: center; margin-left: 50%; margin-right: auto; margin-top: 5%">
+    <a class="topLevel text" id="createRoom" ><span class="text-center raise boxed headertext text">Create</span></a>
+    <img src="/static/images/leftaux.svg" class="img-responsive" style="position: sticky; margin-top: 10%;">
+  </div>
+  <br><br>
+    <h2 class="text middle">OR</h2>
+    <br><br>
+      <div style="display: flex; justify-content: center; margin-right: 50%; margin-left: auto;">
+        <img style="position: sticky;" class="img-responsive text" src="/static/images/rightaux.svg" alt="">
+        <a class="text" id="joinRoom"><span class="text-center raise boxed text headertext"><span class="text headertext" style="opacity: 0">1</span>Join<span class="text headertext" style="opacity: 0">1</span></span></a>
+      </div>
+    </div>`;
     $('.wrapper').empty();
     $('.wrapper').append(home);
   });
