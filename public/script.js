2acbe18158da39ec7236be6e1bebf02355d1511a
yashnevatia <yash1997@hku.hk>
2017-07-14 17:27:17 -0700

    here

--- public/script.js
+++ public/script.js
@@ -109,29 +109,12 @@ $(document).ready(function() {
         listofRooms.push(key);
       }
     }
-    function myFunction() {
-        // Declare variables
-        var input, filter, ul, li, a, i;
-        input = document.getElementById('myInput');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("listofRooms");
-        li = ul.getElementsByTagName('li');
-
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-            a = li[i].getElementsByTagName("button")[0];
-            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                li[i].style.display = "";
-            } else {
-                li[i].style.display = "none";
-            }
-        }
-    };
+    
 
     console.log(listofRooms);
     $('.wrapper').empty();
     $('.wrapper').append(`<h3 style="color:white">List of available rooms</h3>`);
-    $('.wrapper').append(`<input type="text" id="myInput" onkeyup="myFunction()"
+    $('.wrapper').append(`<input type="text" id="myInput" onkeyup="searchFunction()"
     placeholder="Search for names..">`)
     $('.wrapper').append(`<ul id="listofRooms"> </ul>`);
 
